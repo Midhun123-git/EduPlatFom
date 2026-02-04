0db576ad6824d299a39d491a10feb2cc2bc3fbae
Midhun <midhunmadhuuu@gmail.com>
2026-02-04 18:36:40 +0530

    Assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
-
+import Header from './Componets/Header'
+import { Routes,Route } from 'react-router-dom'
+import Hero from './Componets/Hero'
+import Programs from './Componets/Programs'
+import PopularCourses from './Componets/PopularCourses'
+import PopularCoursesPage from './Pages/PopularCoursesPage'
+import Trust from './Pages/Trust'
 const App = () => {
   return (
-    <div>App</div>
+    
+    <Routes>
+      <Route path="/Header" element={<Header/>}/>
+      <Route path="/Hero" element={<Hero/>}/>
+      <Route path="/Programs" element={<Programs/>}/>
+      <Route path="/PopularCoursesPage" element={<PopularCoursesPage/>}/>
+       <Route path="/Trust" element={<Trust/>}/>
+       {/* <Route path="/PopularCourses" element={<PopularCourses/>}/> */}
+    </Routes>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,50 @@ import React from "react"
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom"
+import Sub from '../src/assets/Sub.png'
+import { HiOutlineBookOpen } from "react-icons/hi2";
 import './index.css'
+import Programs from "./Componets/Programs.jsx"
 import App from './App.jsx'
-import Header from "./Componets/Header.jsx"
+import PopularCourses from "./Componets/PopularCourses.jsx";
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
    <BrowserRouter>
-        {/* <App /> */}
-       <Header/>
-  
+        <App />
+        {/* <Programs
+      icon={<HiOutlineBookOpen size={35} />}
+      iconBg="#3B82F6"
+      title="School Tuition"
+      description=
+      {
+        <>
+          Classes 4-12 covering CBSE, ICSE,<br/> And State syllabi
+        </>
+      }
+     
+      point1="Interactive Live Classes"
+      point2="Doubt Clearing Sessions"
+      point3="Regular Assessments"
+         /> */}
+         {/* <PopularCourses
+  image={Sub}
+  discount="40% OFF"
+  popular="Popular"
+  level="Advanced"
+  rate="4.8"
+  review="(12,500)"
+  description={
+    <>
+      Complete Mathematics for <br /> Class 10 CBSE
+    </>
+  }
+  professor="Dr. Rajesh Kumar"
+  duration="6 Months"
+  lessons="180 Lessons"
+  price="₹2,999"
+  oldprice="₹8,999"
+/> */}
+
    </BrowserRouter>
    </React.StrictMode>,
     
